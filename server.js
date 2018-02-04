d855a74361e88d2e8a8d21324d75c65da9c100ee
gbmahili <georgesmahili@gmail.com>
2018-02-04 18:28:24 -0500

    Required all dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+// App Dependencies
+var express, bodyParser, path;
+express = require("express");
+bodyParser = require("body-parser");
+path = require("path");
+// Initiate app
+var app, PORT;
+app = express();
+PORT = 3000;
