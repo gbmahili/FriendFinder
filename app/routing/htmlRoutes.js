89d60db453562d5b49e62dea05fa2fb603d5d230
gbmahili <georgesmahili@gmail.com>
2018-02-04 19:08:48 -0500

    Added the htmlRoutes.js

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// App Dependencies
+var express, bodyParser, path;
+express = require("express");
+bodyParser = require("body-parser");
+path = require("path");
+// Initiate app
+var app, PORT;
+app = express();
+PORT = 3000;
+// Use bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Home route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// Start server
+app.listen(PORT, ()=>{console.log(`Listening to: ${PORT}`)});
