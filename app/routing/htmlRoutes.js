8ab336acbb5aeb5d9e45a12fad94316fe9350f9a
gbmahili <georgesmahili@gmail.com>
2018-02-04 19:36:24 -0500

    Added route for home

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 // Home route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(`${__dirname}/../public/`, "home.html"));
 });
 
 // Start server
